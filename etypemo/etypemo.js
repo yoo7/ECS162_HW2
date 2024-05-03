cba388bff654cee5ea92a63a38fee8f9f0ed73ad
yoo7 <ybcjin@ucdavis.edu>
2024-05-02 19:24:56 -0700

    etypemo comments added

--- etypemo/etypemo.js
+++ etypemo/etypemo.js
@@ -136,40 +136,12 @@ const words = {
 window.addEventListener("load", init);
 
 function init() {
+  // Define some variables that the various functions can access
   const startModal = document.querySelector("#start-game-prompt");
   startModal.style.display = "block";
 
   const startButton = document.querySelector("button#start-game");
 
-  function continueGame() {
-    startModal.style.display = "none";
-    startGame();
-  }
-
-  startButton.addEventListener("click", () => {
-    continueGame();
-
-    if (startButton.innerText !== "Play Again") {
-      startButton.innerText = "Play Again";
-    }
-  });
-
-  window.addEventListener("keypress", (event) => {
-    if (event.key !== "Enter") {
-      return;
-    }
-
-    if (startModal.style.display === "block") {
-      continueGame();
-    }
-  });
-
-  // The following used to be from game.js
-  function _getRandomFromArray(array) {
-    return array[Math.floor(Math.random() * array.length)];
-  }
-  
-  // get DOM elements
   const wordEl = document.querySelector("#word-guess");
   const inputEl = document.querySelector("#word-input");
   const timeEl = document.querySelector("#time");
@@ -187,31 +159,85 @@ function init() {
   let difficulty = _getRandomFromArray(difficulties);
   let wordCount = 0;
   let wordToGuess;
+
+  window.addEventListener("keypress", (event) => {
+    // Ignore keypresses that aren't the Enter button
+    if (event.key !== "Enter") {
+      return;
+    }
+
+    // If the pop-up screen is visible, then pressing Enter will start the game
+    if (startModal.style.display === "block") {
+      continueGame();
+    }
+  });
+
+  /*
+   * If the user presses the start button (rather than pressing enter -- see window listener),
+   * Then start the game. If the user is starting the game for the first time,
+   * then change button content to say "Play Again" 
+   * (updating in advance to be used when the game is finished) 
+   */
+  startButton.addEventListener("click", () => {
+    continueGame();
+
+    if (startButton.innerText !== "Play Again") {
+      startButton.innerText = "Play Again";
+    }
+  });
+
+  /**
+   * To start the game, remove the pop-up screen and start the game
+   */
+  function continueGame() {
+    startModal.style.display = "none";
+    startGame();
+  }
+
+  // NEW: The following code used to be from the original game.js, but we moved all the js files into one
+  /**
+   * Select a random number, then index into the array (of words from the same category)
+   * to get a random word from that category
+   */
+  function _getRandomFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
   
+  /** Update the time based on what current timeSeconds value is */
   function updateTime() {
     timeEl.innerText = timeSeconds;
   }
   
+  /** Update the word to type based on what the current wordToGuess is */
   function updateWord() {
     wordEl.innerText = wordToGuess;
   }
   
+  /** Display the new difficulty */
   function updateDifficulty() {
     difficultyEl.innerText = "Difficulty: " + difficulty;
   }
   
+  /** Update the total # of words successfully typed */
   function updateWordCount() {
     wordCountEl.innerText = "Word count: " + wordCount;
   }
   
+  /** Turn off/disable timer */
   function resetInterval() {
+    // If timer is valid/enabled
     if (interval) {
+      /*
+       * Stop the timer (which updated the time every second), and disable it
+       * since we'll check the value of interval in other places
+       */
       clearInterval(interval);
       interval = null;
     }
   }
   
   function reset() {
+    // Setup game state for another round
     wordCount = 0;
     timeSeconds = 11;
     difficulty = "Easy";
@@ -219,46 +245,60 @@ function init() {
   }
   
   function lose() {
+    // Turn off/disable timer
     resetInterval();
   
+    // Reveal pop-up screen
     modal.style.display = "block";
     modalP.innerText = "Your score is " + wordCount;
   
+    // Nothing in the input box (where user types)
     inputEl.value = "";
   
+    // If this score isn't in the list yet, then add it
     if (!scores.includes(wordCount)) {
       scores.push(wordCount);
     }
   
+    // Set up for next game
     reset();
   
     localStorage.setItem(
       "scores",
       JSON.stringify(scores)
     );
-  
   }
   
+  /** Start the timer */
   function startTime() {
     function countdown() {
+      // Decrease num seconds by 1 and update on the DOM/screen
       timeSeconds--;
       updateTime();
   
+      // Ran out of time
       if (timeSeconds <= 0) {
         lose();
       }
     }
   
+    // Call the countdown() function every second and return interval ID unique to this timer
     interval = setInterval(countdown, 1000);
   }
   
+  /** Reveal the next word */
   function nextWord() {
+    // Update word count
     wordCount++;
     updateWordCount();
+
+    // Disable timer for now
     resetInterval();
   
+    // Nothing in user input box
     inputEl.value = "";
   
+    // Pick random difficulty and add seconds to <current amount of seconds left>
     difficulty = _getRandomFromArray(difficulties);
   
     timeSeconds =
@@ -270,8 +310,10 @@ function init() {
         ? timeSeconds + 2
         : timeSeconds + 1;
   
+    // Pick random word from that category to use
     wordToGuess = _getRandomFromArray(words[difficulty]);
   
+    // Update DOM/screen and restart the timer
     updateDifficulty();
     updateWord();
     startTime();
@@ -280,11 +322,14 @@ function init() {
   
   updateTime();
   
+  /** Pick word and do general setup */
   function startGame() {
+    // Pick a random word from the category that was randomly chosen in the declaration of difficulty
     wordToGuess = _getRandomFromArray(words[difficulty]);
   
     bestScoreEl.innerText = "Best: " + Math.max(...scores);
   
+    // User focus is already on the input box
     inputEl.value = "";
     inputEl.focus();
   
@@ -294,6 +339,7 @@ function init() {
     updateDifficulty();
   }
   
+  // Check if the user's input matches the word to type
   inputEl.addEventListener("input", (event) => {
     if (event.target.value.toLowerCase().trim() === wordToGuess.toLowerCase()) {
       nextWord()
